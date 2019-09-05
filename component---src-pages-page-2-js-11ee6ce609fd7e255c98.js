6e6e638d65016dcc69fafe10f036adbeee462f3f
Nick Flanders <ngflanders@gmail.com>
2019-09-04 22:19:36 -0500

    Updates

---
+++ component---src-pages-page-2-js-11ee6ce609fd7e255c98.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{213:function(e,t,a){"use strict";a.r(t);var n=a(1),o=a.n(n),l=a(102),c=a(211),r=a(210);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-11ee6ce609fd7e255c98.js.map
